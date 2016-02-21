02ee767fb09e88d081ef2920f3339bb0e08737ef
Christophe Hurpeau <christophe@hurpeau.com>
2016-02-21 21:28:40 +0100

    fieldset config

--- src/views/components/RaspberryComponent.jsx
+++ src/views/components/RaspberryComponent.jsx
@@ -3,7 +3,7 @@ import shouldPureComponentUpdate from 'react-pure-render/function';
 import Spinner from './SpinnerComponent';
 
 const actions = [
-    { name: 'Blink', value: 'blink', isVisible: r => true },
+    // { name: 'Test', value: 'test', isVisible: r => true },
     { name: 'Refresh', value: 'refresh', isVisible: r => r.screenState === 'on' },
     { name: 'Screen on', value: 'screen-on', isVisible: r => r.screenState === 'off' },
     { name: 'Screen off', value: 'screen-off', isVisible: r => r.screenState === 'on' },
@@ -43,7 +43,20 @@ export default class RaspberryComponent extends Component {
             </h2>
             <Spinner active={raspberry.saving} />
 
-            {!raspberry.online || !actions.length ? '' :
+            <div className="status-container">
+                <span className={`raspberry-status label ${raspberry.online ? 'success' : 'warning'}`}>
+                    {raspberry.online ? `${raspberry.ip} ${raspberry.online}` : 'Offline'}
+                </span>
+                {
+                    !raspberry.online ? '' :
+                    <span className="screen-status" title={raspberry.screenState === 'on' ? 'On' : 'Off'}>
+                        <span className="icon" />
+                        <span className={`status ${raspberry.screenState}`} />
+                    </span>
+                }
+            </div>
+
+            {!raspberry.online || !availableActions.length ? '' :
                 <div className="actions">
                     <div className="dropdown button">
                         Actions
@@ -61,38 +74,45 @@ export default class RaspberryComponent extends Component {
                 </div>
             }
 
-            <div className="status-container">
-                <span className={`raspberry-status label ${raspberry.online ? 'success' : 'warning'}`}>
-                    {raspberry.online ? `${raspberry.ip} ${raspberry.online}` : 'Offline'}
-                </span>
-                {
-                    !raspberry.online ? '' :
-                    <span className="screen-status" title={raspberry.screenState === 'on' ? 'On' : 'Off'}>
-                        <span className="icon" />
-                        <span className={`status ${raspberry.screenState}`} />
-                    </span>
-                }
-            </div>
+            <fieldset>
+                <legend>Config</legend>
+                <div className="row row-responsive spaced">
+                    <div className="col" style={{ width: '100px', 'flex-basis': '100px', 'flex-grow': 0 }}>
+                        <div className="input select">
+                            <select
+                                id={`raspberry-select-${raspberry.id}`}
+                                className={`has-value`}
+                            >
+                                <option>kweb3</option>
+                            </select>
+                            <label htmlFor={`raspberry-select-${raspberry.id}`}>Display</label>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="input text">
+                            <input
+                                id={`raspberry-url-${raspberry.id}`}
+                                type="url" required
+                                className={`has-value${url ? '' : ' has-empty-value'}`}
+                                value={url}
+                                autoComplete="off"
+                                onChange={(e) => this.setState({
+                                    url: raspberry.data.config.url === e.target.value ? null : e.target.value
+                                })}
+                            />
+                            <label htmlFor={`raspberry-url-${raspberry.id}`}>URL</label>
+                        </div>
+                    </div>
+                </div>
 
-            <div className="input text">
-                <input type="url" required
-                    className={`has-value${url ? '' : ' has-empty-value'}`}
-                    value={url}
-                    autoComplete="off"
-                    onChange={(e) => this.setState({
-                        url: raspberry.data.config.url === e.target.value ? null : e.target.value
-                    })}
-                />
-                <label htmlFor={`raspberry-url-${raspberry.id}`}>URL</label>
-            </div>
-
-            <div className="button-container">
-                <button type="button" disabled={raspberry.saving || !this.state.url} onClick={() => {
-                    const url = this.state.url;
-                    this.setState({ url: null, lastUrl: url });
-                    changeConfig(raspberry, { url });
-                }}>Save</button>
-            </div>
+                <div className="button-container center">
+                    <button type="button" disabled={raspberry.saving || !this.state.url} onClick={() => {
+                        const url = this.state.url;
+                        this.setState({ url: null, lastUrl: url });
+                        changeConfig(raspberry, { url });
+                    }}>Save</button>
+                </div>
+            </fieldset>
         </div>);
     }
 }
