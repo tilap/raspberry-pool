5b7e211f9839808a8671bb8a613047e3fb1e423e
DarzuL <guillaumebourderye@gmail.com>
2016-03-17 18:31:19 +0100

    Fix install-raspberry.sh

--- public/install-scripts/install-raspberry.sh
+++ public/install-scripts/install-raspberry.sh
@@ -1,139 +1,37 @@
-checkRoot() {
-	if [ $USER != 'root' ]; then
-		echo "You are not root, please execute this script with sudo."
-		exit 1
-	fi
-}
+#!/bin/sh
 
-displayTitle() {
+URL=http://192.168.1.122:3100/install-scripts/
+
+display_title() {
 	echo "\033[33m> \033[1m" $1 "\033[0m"
 }
 
+display_title "Configure time zone"
+sudo dpkg-reconfigure tzdata
 
-### raspi-config is licensed under the terms of the MIT license. Copyright (c) 2012 Alex Bradbury <asb@asbradbury.org>
-INTERACTIVE=True
+display_title "Expand root file system"
+wget $URL"expand_rootfs.sh"
+chmod +x expand_rootfs.sh
+sudo ./expand_rootfs.sh
+rm expand_rootfs.sh
 
-do_expand_rootfs() {
-  if ! [ -h /dev/root ]; then
-    whiptail --msgbox "/dev/root does not exist or is not a symlink. Don't know how to expand" 20 60 2
-    return 0
-  fi
+display_title "Enable auto login"
+wget $URL"autologin.sh"
+chmod +x autologin.sh
+sudo ./autologin.sh enable
 
-  ROOT_PART=$(readlink /dev/root)
-  PART_NUM=${ROOT_PART#mmcblk0p}
-  if [ "$PART_NUM" = "$ROOT_PART" ]; then
-    whiptail --msgbox "/dev/root is not an SD card. Don't know how to expand" 20 60 2
-    return 0
-  fi
+display_title "Prepare rpi-update after reboot"
+wget $URL"rpi-update.sh"
+chmod +x rpi-update.sh
 
-  # NOTE: the NOOBS partition layout confuses parted. For now, let's only
-  # agree to work with a sufficiently simple partition layout
-  if [ "$PART_NUM" -ne 2 ]; then
-    whiptail --msgbox "Your partition layout is not currently supported by this tool. You are probably using NOOBS, in which case your root filesystem is already expanded anyway." 20 60 2
-    return 0
-  fi
+wget $URL"apt.sh"
+chmod +x apt.sh
 
-  LAST_PART_NUM=$(parted /dev/mmcblk0 -ms unit s p | tail -n 1 | cut -f 1 -d:)
+wget $URL"install-client.sh"
+chmod +x install-client.sh
 
-  if [ "$LAST_PART_NUM" != "$PART_NUM" ]; then
-    whiptail --msgbox "/dev/root is not the last partition. Don't know how to expand" 20 60 2
-    return 0
-  fi
+echo "./installer.sh" >> .profile
+echo "./rpi-update.sh" > installer.sh
 
-  # Get the starting offset of the root partition
-  PART_START=$(parted /dev/mmcblk0 -ms unit s p | grep "^${PART_NUM}" | cut -f 2 -d:)
-  [ "$PART_START" ] || return 1
-  # Return value will likely be error for fdisk as it fails to reload the
-  # partition table because the root fs is mounted
-  fdisk /dev/mmcblk0 <<EOF
-p
-d
-$PART_NUM
-n
-p
-$PART_NUM
-$PART_START
-p
-w
-EOF
-
-  # now set up an init.d script
-cat <<\EOF > /etc/init.d/resize2fs_once &&
-#!/bin/sh
-### BEGIN INIT INFO
-# Provides:          resize2fs_once
-# Required-Start:
-# Required-Stop:
-# Default-Start: 2 3 4 5 S
-# Default-Stop:
-# Short-Description: Resize the root filesystem to fill partition
-# Description:
-### END INIT INFO
-. /lib/lsb/init-functions
-case "$1" in
-  start)
-    log_daemon_msg "Starting resize2fs_once" &&
-    resize2fs /dev/root &&
-    rm /etc/init.d/resize2fs_once &&
-    update-rc.d resize2fs_once remove &&
-    log_end_msg $?
-    ;;
-  *)
-    echo "Usage: $0 start" >&2
-    exit 3
-    ;;
-esac
-EOF
-  chmod +x /etc/init.d/resize2fs_once &&
-  update-rc.d resize2fs_once defaults &&
-  if [ "$INTERACTIVE" = True ]; then
-    whiptail --msgbox "Root partition has been resized.\nThe filesystem will be enlarged upon the next reboot" 20 60 2
-  fi
-}
-
-### END raspi-config
-
-
-checkRoot
-
-dpkg-reconfigure tzdata
-dpkg-reconfigure locales
-do_expand_rootfs
-apt-get install rpi-update
-rpi-update
-
-apt-get update
-apt-get upgrade -y
-
-displayTitle 'Disable desktop manager at boot'
-update-rc.d lightdm disable
-
-displayTitle 'Allow any user to start X'
-sed -i 's/^allowed_users=.*$/allowed_users=anybody/g' /etc/X11/Xwrapper.config
-
-
-# Install client-cec
-
-apt-get install -y git cmake liblockdev1-dev libudev-dev libxrandr-dev python-dev swig
-
-cd
-git clone https://github.com/Pulse-Eight/platform.git
-mkdir platform/build
-cd platform/build
-cmake ..
-make
-make install
-
-cd
-git clone https://github.com/Pulse-Eight/libcec.git
-mkdir libcec/build
-cd libcec/build
-cmake -DRPI_INCLUDE_DIR=/opt/vc/include -DRPI_LIB_DIR=/opt/vc/lib ..
-make -j4
-make install
-ldconfig
-
-cd
-rm -rf libcec platform
-
+rm $0
 reboot
