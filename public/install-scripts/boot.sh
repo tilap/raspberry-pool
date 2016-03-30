34af7f53ae3e34ae57ce595a9512d5399ed37022
DarzuL <guillaumebourderye@gmail.com>
2016-03-30 14:58:28 +0200

    Improve regex to remove boot script in .profile file

--- public/install-scripts/boot.sh
+++ public/install-scripts/boot.sh
@@ -5,21 +5,20 @@ BOOT_SCRIPT="installer.sh"
 case $1 in
     # The BOOT_SCRIPT is executed just after boot
     enable)
-        touch $BOOT_SCRIPT
         echo "sh $BOOT_SCRIPT" >> .profile
     ;;
 
     # The BOOT_SCRIPT isn't execute anymore
     disable)
         rm $BOOT_SCRIPT
-        sed -i 's/^sh '$BOOT_SCRIPT$'//g' .profile
+        sed -i 's/^sh '$BOOT_SCRIPT'.*$//g' .profile
     ;;
 
     # Rewrite the BOOT_SCRIPT
     next)
         echo $2 > installer.sh
     ;;
-
+        
     *)
         echo "Usage: $0 (enable|disable|next command)"
         exit 1
