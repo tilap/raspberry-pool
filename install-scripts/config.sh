3636a5b8813e54b068545362fd3e2dd60b886449
DarzuL <guillaumebourderye@gmail.com>
2016-03-31 12:03:02 +0200

    Quick fix

--- install-scripts/config.sh
+++ install-scripts/config.sh
@@ -2,9 +2,9 @@
 
 export NODEJS_VERSION='5.8.0'
 
-export ENABLE_SSH_KEY_AUTH='false'
+export ENABLE_SSH_KEY_AUTH=false
 export SSH_PUBLIC_KEYS=''
 
-export ENABLE_WIFI='false'
+export ENABLE_WIFI=false
 export WIFI_SSID=''
 export WIFI_PASS=''
