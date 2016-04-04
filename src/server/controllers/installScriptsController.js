ee4614d494dd95ba967681839bdf7b62751c05a4
Christophe Hurpeau <christophe@hurpeau.com>
2016-04-04 17:17:13 +0200

    fix tcp port in install-client

--- src/server/controllers/installScriptsController.js
+++ src/server/controllers/installScriptsController.js
@@ -24,7 +24,7 @@ export default newController({
         } else if (scriptName === 'install-client') {
             scriptBody = scriptBody.replace(
                 CONFIG_PLACEHOLDER,
-                `SERVER_HOSTNAME="${ctx.request.origin}"\nSERVER_PORT=${ctx.app.config.get('tcpSocketPort')}`
+                `SERVER_HOSTNAME="${ctx.request.origin}"\nSERVER_PORT=${ctx.app.config.get('tcpSocket').get('port')}`
             );
         }
 
